502b9d60456db6877c87b19d07778684a1587f6c
CoreLabs31 <corelabs.pc@outlook.com>
2025-01-29 00:50:44 +0400

    Update node_test.py

--- node/node_test.py
+++ node/node_test.py
@@ -1,1 +1,21 @@
+import unittest
+from node.node import Node
+from orge.blockchain import Blockchain
+
+class TestNode(unittest.TestCase):
+    def setUp(self):
+        self.blockchain = Blockchain()
+        self.node = Node(self.blockchain, port=5000)
+
+    def test_add_peer(self):
+        self.node.peers.add("http://127.0.0.1:5001")
+        self.assertIn("http://127.0.0.1:5001", self.node.peers)
+
+    def test_get_chain(self):
+        with self.node.app.test_client() as client:
+            response = client.get('/chain')
+            self.assertEqual(response.status_code, 200)
+
+if __name__ == "__main__":
+    unittest.main()
 
